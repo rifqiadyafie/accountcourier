68a1359a7b75007ab11705bf5ce871ad1c27e733
rifqiadyafie <mo.rifqiadyafie@gmail.com>
2023-03-22 18:12:05 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,791 @@
+var couriers = [
+        {
+        "courier": 107357,
+        "name": "KGX TRISNA SANDI WILIYAN"
+    },
+    {
+        "courier": 110881,
+        "name": "KGX AEPUDIN"
+    },
+    {
+        "courier": 111167,
+        "name": "KGX SURYADI"
+    },
+    {
+        "courier": 139150,
+        "name": "KGX ADUNG AFRIANTO"
+    },
+    {
+        "courier": 110384,
+        "name": "KGX ADANG SURYANA"
+    },
+    {
+        "courier": 107356,
+        "name": "KGX MUHAMAD SYAHAB"
+    },
+    {
+        "courier": 149224,
+        "name": "KGX DENDI RAMADHANI"
+    },
+    {
+        "courier": 140838,
+        "name": "KGX RITA"
+    },
+    {
+        "courier": 102900,
+        "name": "KGX ARIFIN NUGROHO"
+    },
+    {
+        "courier": 152578,
+        "name": "KGX DIYO ETGARSITO"
+    },
+    {
+        "courier": 139151,
+        "name": "KGX WAHYUDI"
+    },
+    {
+        "courier": 136066,
+        "name": "KGX IRWANTO"
+    },
+    {
+        "courier": 160086,
+        "name": "KGX SAEFUL ANWAR"
+    },
+    {
+        "courier": 172162,
+        "name": "KGX INU YASA"
+    },
+    {
+        "courier": 103766,
+        "name": "KGX AJI ATAMA"
+    },
+    {
+        "courier": 176094,
+        "name": "KGX WAHYUDI NEW"
+    },
+    {
+        "courier": 110389,
+        "name": "KGX AMIN HASBI"
+    },
+    {
+        "courier": 103734,
+        "name": "KGX KERUL SOMALUDIN"
+    },
+    {
+        "courier": 103042,
+        "name": "KGX LEO AGUSTA"
+    },
+    {
+        "courier": 111111,
+        "name": "KGX SUGIONO"
+    },
+    {
+        "courier": 135091,
+        "name": "KGX SYARIFULLAH"
+    },
+    {
+        "courier": 102946,
+        "name": "KGX DIAN INDRA PERMANA"
+    },
+    {
+        "courier": 111103,
+        "name": "KGX AGUS SUDIRMAN"
+    },
+    {
+        "courier": 103038,
+        "name": "KGX ALFIN FIRDAUS"
+    },
+    {
+        "courier": 102986,
+        "name": "KGX ARIF MUCHABIBI"
+    },
+    {
+        "courier": 103060,
+        "name": "KGX BUDI HARYANTO"
+    },
+    {
+        "courier": 154755,
+        "name": "KGX TARYANA"
+    },
+    {
+        "courier": 154755,
+        "name": "KGX MUHAMMAD ARIEF HIDAYAT"
+    },
+    {
+        "courier": 124044,
+        "name": "KGX ANDRE MAKHMUD"
+    },
+    {
+        "courier": 103750,
+        "name": "KGX SARIP HIDAYAT"
+    },
+    {
+        "courier": 103033,
+        "name": "KGX ANWARUDIN"
+    },
+    {
+        "courier": 135090,
+        "name": "KGX CECEP ASROHI"
+    },
+    {
+        "courier": 149227,
+        "name": "KGX TOTO NURKANTO"
+    },
+    {
+        "courier": 124975,
+        "name": "KGX WAWAN ERAWAN"
+    },
+    {
+        "courier": 111703,
+        "name": "KGX LUKMANUL HAKIM"
+    },
+    {
+        "courier": 102990,
+        "name": "KGX ASEP JUANDI"
+    },
+    {
+        "courier": 107369,
+        "name": "KGX CASMANTO"
+    },
+    {
+        "courier": 107379,
+        "name": "KGX SUTARNO"
+    },
+    {
+        "courier": 126697,
+        "name": "KGX RASTA"
+    },
+    {
+        "courier": 114483,
+        "name": "KGX NARTA"
+    },
+    {
+        "courier": 107380,
+        "name": "KGX BRAMMANTIO URAMBOLY"
+    },
+    {
+        "courier": 102969,
+        "name": "KGX KRISMANTO"
+    },
+    {
+        "courier": 111707,
+        "name": "KGX HELLY SATRIYADI"
+    },
+    {
+        "courier": 138170,
+        "name": "KGX HERU WIJAYAKUSUMA"
+    },
+    {
+        "courier": 110404,
+        "name": "KGX KRISNA DENADA"
+    },
+    {
+        "courier": 110903,
+        "name": "KGX ALIM MUALIM"
+    },
+    {
+        "courier": 111728,
+        "name": "KGX ANDI RISDIANDI ALAMSYAH"
+    },
+    {
+        "courier": 103023,
+        "name": "KGX CARNITA"
+    },
+    {
+        "courier": 149226,
+        "name": "KGX DENI SUPRIYADI"
+    },
+    {
+        "courier": 102850,
+        "name": "KGX ARIEF ILHAMSYAH"
+    },
+    {
+        "courier": 170400,
+        "name": "KGX OKI BADAY"
+    },
+    {
+        "courier": 154494,
+        "name": "KGX ZAENAL MASRUR"
+    },
+    {
+        "courier": 116795,
+        "name": "KGX Riyandi"
+    },
+    {
+        "courier": 112408,
+        "name": "KGX AHMAD TAUFIK"
+    },
+    {
+        "courier": 103736,
+        "name": "KGX KASYANTO"
+    },
+    {
+        "courier": 153626,
+        "name": "KGX SURAJAB"
+    },
+    {
+        "courier": 107372,
+        "name": "KGX ZULFAN YAHYA"
+    },
+    {
+        "courier": 103743,
+        "name": "KGX YANA WAHADI."
+    },
+    {
+        "courier": 102982,
+        "name": "KGX FATKHUR ROZI DWI TAMA"
+    },
+    {
+        "courier": 112409,
+        "name": "KGX MUHAMMAD ASYROFI"
+    },
+    {
+        "courier": 174735,
+        "name": "KGX KANIMAN"
+    },
+    {
+        "courier": 154782,
+        "name": "KGX RIFAN ANAS"
+    },
+    {
+        "courier": 102984,
+        "name": "KGX WAGINO"
+    },
+    {
+        "courier": 139950,
+        "name": "KGX MUHAMMAD FAQIH AL AYUBI"
+    },
+    {
+        "courier": 102975,
+        "name": "KGX CAHADI"
+    },
+    {
+        "courier": 153618,
+        "name": "KGX DWI NOPANDI"
+    },
+    {
+        "courier": 112416,
+        "name": "KGX ISMANTO"
+    },
+    {
+        "courier": 103739,
+        "name": "KGX AIDI SUBADA"
+    },
+    {
+        "courier": 116798,
+        "name": "KGX KHOLID MAWARD"
+    },
+    {
+        "courier": 139942,
+        "name": "KGX ADI SUWADI"
+    },
+    {
+        "courier": 103030,
+        "name": "KGX NABIL FATHUR RAHMAN"
+    },
+    {
+        "courier": 169343,
+        "name": "KGX RIZKI AJI TRIONO"
+    },
+    {
+        "courier": 116797,
+        "name": "KGX ABDUL HALIM NEW"
+    },
+    {
+        "courier": 107373,
+        "name": "KGX DIKY INDRAWAN"
+    },
+    {
+        "courier": 103056,
+        "name": "KGX IKHBAL AGRO BISNIS"
+    },
+    {
+        "courier": 114499,
+        "name": "KGX BAGUS PRABOWO"
+    },
+    {
+        "courier": 103077,
+        "name": "KGX ARIE PRASETYO"
+    },
+    {
+        "courier": 102983,
+        "name": "KGX FEBRY SUGIANTO"
+    },
+    {
+        "courier": 112412,
+        "name": "KGX DIDI SUWANDI"
+    },
+    {
+        "courier": 102947,
+        "name": "KGX MEGAH BAGUS MEI DOLA"
+    },
+    {
+        "courier": 110381,
+        "name": "KGX ADE ARYONO"
+    },
+    {
+        "courier": 140839,
+        "name": "KGX SUWARDI"
+    },
+    {
+        "courier": 110880,
+        "name": "KGX AMRI HADI RESTU"
+    },
+    {
+        "courier": 109249,
+        "name": "KGX AHMAD MAHENDRA"
+    },
+    {
+        "courier": 107358,
+        "name": "KGX TIKO HARYANTO"
+    },
+    {
+        "courier": 103758,
+        "name": "KGX NANANG CARUDIN"
+    },
+    {
+        "courier": 103733,
+        "name": "KGX DARSIKA"
+    },
+    {
+        "courier": 108161,
+        "name": "KGX DION PRAMONO"
+    },
+    {
+        "courier": 149225,
+        "name": "KGX DEDE HARI WIGUNA"
+    },
+    {
+        "courier": 110379,
+        "name": "KGX TARMO"
+    },
+    {
+        "courier": 103045,
+        "name": "KGX PRAMUGIANTO"
+    },
+    {
+        "courier": 111727,
+        "name": "KGX MUHAMMAD ZAKKI"
+    },
+    {
+        "courier": 111136,
+        "name": "KGX DAROJI"
+    },
+    {
+        "courier": 149223,
+        "name": "KGX AGUS SALIM"
+    },
+    {
+        "courier": 107368,
+        "name": "KGX ALAN SEPTIADI"
+    },
+    {
+        "courier": 103047,
+        "name": "KGX ADE SURYANA"
+    },
+    {
+        "courier": 110406,
+        "name": "KGX JUNANI MUHAMMAD SLAMET"
+    },
+    {
+        "courier": 102890,
+        "name": "KGX SUPARDI"
+    },
+    {
+        "courier": 103050,
+        "name": "KGX ERNANTO"
+    },
+    {
+        "courier": 110892,
+        "name": "KGX JAKY JUMANTO"
+    },
+    {
+        "courier": 107362,
+        "name": "KGX BUDIMAN"
+    },
+    {
+        "courier": 103754,
+        "name": "KGX AKHBIL AGRI BISNIS"
+    },
+    {
+        "courier": 164765,
+        "name": "KGX JAMALUDIN KHUSEN"
+    },
+    {
+        "courier": 105720,
+        "name": "KGX TITO HIDAYAT"
+    },
+    {
+        "courier": 103053,
+        "name": "KGX ADE ROMANDA SE"
+    },
+    {
+        "courier": 152581,
+        "name": "KGX RIYAN PEBRIYANTO"
+    },
+    {
+        "courier": 102965,
+        "name": "KGX HADI PURNAMA"
+    },
+    {
+        "courier": 172606,
+        "name": "KGX TRI INDRA GUNAWAN"
+    },
+    {
+        "courier": 107381,
+        "name": "KGX RYAN BUDI PRAKOSO"
+    },
+    {
+        "courier": 155688,
+        "name": "KGX DIKY PRIYONO"
+    },
+    {
+        "courier": 127129,
+        "name": "KGX RISALAM"
+    },
+    {
+        "courier": 124973,
+        "name": "KGX FAZRUR RAHMAN ARUMI"
+    },
+    {
+        "courier": 102985,
+        "name": "KGX KHOIRUDIN KHUMAIDI"
+    },
+    {
+        "courier": 103064,
+        "name": "KGX ADI CISWOYO"
+    },
+    {
+        "courier": 154496,
+        "name": "KGX M. ILHAM AZIS PERMANA"
+    },
+    {
+        "courier": 164763,
+        "name": "KGX SAMSUL ARIF TOHIDI"
+    },
+    {
+        "courier": 102943,
+        "name": "KGX FATUROHMAN"
+    },
+    {
+        "courier": 103730,
+        "name": "KGX SOFYAN RIYADI"
+    },
+    {
+        "courier": 103068,
+        "name": "KGX EDI"
+    },
+    {
+        "courier": 103024,
+        "name": "KGX MUJIYANTO"
+    },
+    {
+        "courier": 111734,
+        "name": "KGX GEBYAR KASIYAN"
+    },
+    {
+        "courier": 139944,
+        "name": "KGX SUCHI ANGGREANA"
+    },
+    {
+        "courier": 139953,
+        "name": "KGX BAYU PUTRA PRATAMA"
+    },
+    {
+        "courier": 153614,
+        "name": "KGX MUHAMMAD INDRA PAHLEVI"
+    },
+    {
+        "courier": 133253,
+        "name": "KGX TAUFIK HIDAYAT"
+    },
+    {
+        "courier": 110405,
+        "name": "KGX FATONI"
+    },
+    {
+        "courier": 138174,
+        "name": "KGX WINANDAR"
+    },
+    {
+        "courier": 102974,
+        "name": "KGX RIZKI SUHENDRA"
+    },
+    {
+        "courier": 103751,
+        "name": "KGX SYAMSUL MAARIF"
+    },
+    {
+        "courier": 103070,
+        "name": "KGX TRISNO"
+    },
+    {
+        "courier": 110391,
+        "name": "KGX MUHAMMAD FAISAL"
+    },
+    {
+        "courier": 102963,
+        "name": "KGX AHMAD FAJAR"
+    },
+    {
+        "courier": 102856,
+        "name": "KGX YUSTIAN STEPANUS"
+    },
+    {
+        "courier": 110394,
+        "name": "KGX MUHAMED AL FAIZD DAHLAN"
+    },
+    {
+        "courier": 102962,
+        "name": "KGX SYAEFUL BAKRI"
+    },
+    {
+        "courier": 164766,
+        "name": "KGX MUHAMMAD MAULANA ALI"
+    },
+    {
+        "courier": 107363,
+        "name": "KGX UGI SUGIARTO"
+    },
+    {
+        "courier": 107360,
+        "name": "KGX SYAEFUL MUSTOFA"
+    },
+    {
+        "courier": 103910,
+        "name": "KGX JUMAR MARJUKI"
+    },
+    {
+        "courier": 102957,
+        "name": "KGX FIKRY RANADITYA"
+    },
+    {
+        "courier": 133255,
+        "name": "KGX AGUNG RISNANTO"
+    },
+    {
+        "courier": 139941,
+        "name": "KGX KASNUDI"
+    },
+    {
+        "courier": 102896,
+        "name": "KGX AJIS SOFIANDI"
+    },
+    {
+        "courier": 103058,
+        "name": "KGX ARI APRIYANTO"
+    },
+    {
+        "courier": 103763,
+        "name": "KGX SAPTIAN FEBRISYABAN"
+    },
+    {
+        "courier": 107370,
+        "name": "KGX SAFRUDIN"
+    },
+    {
+        "courier": 107375,
+        "name": "KGX DHICO GUSTIGIANTO LEGYO SAPUTRA"
+    },
+    {
+        "courier": 102993,
+        "name": "KGX TANTO"
+    },
+    {
+        "courier": 111137,
+        "name": "KGX MOH. AFIEF MA'RUF RHAMDANI"
+    },
+    {
+        "courier": 110383,
+        "name": "KGX WIRYO SANTICA"
+    },
+    {
+        "courier": 168485,
+        "name": "KGX MOHAMAD NI'AM FARHI"
+    },
+    {
+        "courier": 124040,
+        "name": "KGX ANDRIO NURHARIPIN"
+    },
+    {
+        "courier": 107374,
+        "name": "KGX ASEP SAEPUDIN"
+    },
+    {
+        "courier": 109240,
+        "name": "KGX MUHAMMAD RIFQI ADYAFIE"
+    },
+    {
+        "courier": 109242,
+        "name": "KGX SAEFUDIN ZUHRI"
+    },
+    {
+        "courier": 103078,
+        "name": "KGX ANDI HARYANTO"
+    },
+    {
+        "courier": 171060,
+        "name": "KGX ADI JULIYANTO"
+    },
+    {
+        "courier": 182183,
+        "name": "KGX SEPRI RAMDANI"
+    },
+    {
+        "courier": 137715,
+        "name": "KGX BUDI SETIANA"
+    },
+    {
+        "courier": 182046,
+        "name": "KGX ROHMAN MUSLIMAN"
+    },
+    {
+        "courier": 127127,
+        "name": "KGX AFIEF ASY SYAFI'I"
+    },
+    {
+        "courier": 102967,
+        "name": "KGX TEGAR SETIANTO"
+    },
+    {
+        "courier": 182044,
+        "name": "KGX MUIJUDIN"
+    },
+    {
+        "courier": 176694,
+        "name": "KGX AEP SAEFUDIN"
+    },
+    {
+        "courier": 184832,
+        "name": "KGX ADIN SARUDIN"
+    },
+    {
+        "courier": 184831,
+        "name": "KGX RIKI LUTFYAWAN FERNANDO"
+    },
+    {
+        "courier": 184834,
+        "name": "KGX RIYANTO ADI KUSUMA"
+    },
+    {
+        "courier": 184075,
+        "name": "KGX MUJAFARUDIN"
+    },
+    {
+        "courier": 184845,
+        "name": "KGX ABDURROHMAN"
+    },
+    {
+        "courier": 103729,
+        "name": "KGX AS'AD SOFYAN"
+    },
+    {
+        "courier": 186385,
+        "name": "KGX JUFRI NUR FADILAH"
+    },
+    {
+        "courier": 186386,
+        "name": "KGX ARYA AHMAD BISRI"
+    },
+    {
+        "courier": 186934,
+        "name": "KGX IBNU UBAEDILAH"
+    },
+    {
+        "courier": 187442,
+        "name": "KGX SLAMET"
+    },
+    {
+        "courier": 187441,
+        "name": "KGX GUNAWAN PERMANA"
+    },
+    {
+        "courier": 187443,
+        "name": "KGX FATTAHUL FAJRIANSYAH"
+    },
+    {
+        "courier": 187744,
+        "name": "KGX SAOKI ZINANI"
+    },
+    {
+        "courier": 188494,
+        "name": "KGX KARNILAH"
+    },
+    {
+        "courier": 188493,
+        "name": "KGX HERUDIN"
+    },
+    {
+        "courier": 188649,
+        "name": "KGX HERDIANA"
+    },
+    {
+        "courier": 189193,
+        "name": "KGX DEDEN JAHUDIN"
+    },
+    {
+        "courier": 189329,
+        "name": "KGX SAPRONI"
+    },
+    {
+        "courier": 189488,
+        "name": "KGX DIYANTO"
+    },
+    {
+        "courier": 198333,
+        "name": "KGX LODHAN HIMAWAN"
+    }
+      ];
+
+      var button = document.getElementById('masuk');
+      const peringatan = document.querySelector('.peringatan');
+      const frlogin = document.querySelector('.login');
+      const fraccount = document.querySelector('.form');
+      const st = document.querySelector('.st');
+      const nd = document.querySelector('.nd');
+
+      button.onclick = function() {
+      var courierID = document.getElementById('id_kurir').value; 
+        
+        for (var i = 0; i < couriers.length; i++) {
+          
+          if(courierID == couriers[i].courier) {
+            // peringatan.classList.remove('hide');
+            frlogin.classList.toggle('hide');
+            fraccount.classList.toggle('hide');
+            st.classList.toggle('hide');
+            nd.classList.toggle('hide');
+            document.getElementById('id').value = couriers[i].courier;
+            document.getElementById('name').value = couriers[i].name;
+            kirim();
+            return;
+          }    
+        }
+        alert('You are trying to break in!');
+        frlogin.reset();
+        location.reload();
+      
+      }
+
+
+
+function kirim() {
+        const scriptURL = 'https://script.google.com/macros/s/AKfycbx3nn3fp7ckkHzm7xiLfwDUNr_CZzUyy7_pRQ8eMyg3bhlpa_B1a9hLDImlfFippvje/exec';
+        const form = document.forms['form-courier'];
+        form.addEventListener('submit', e => {
+          e.preventDefault()
+              fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+                .then(response => {
+                    fraccount.classList.toggle('hide');
+                    nd.classList.toggle('hide');
+                    peringatan.classList.toggle('hide');
+                    form.reset();
+                   // console.log('Success!', response)
+                })
+                .catch(error => {
+                    alert('Pesan anda gagal dikirim.');
+                    form.reset()
+                    console.error('Error!', error.message)
+                })
+            });
+      }
